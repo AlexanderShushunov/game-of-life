fa07510948c3d1dbb5bd5081bf3a5c4afaeef922
Alexandr Shushunov <ashushunov@mail.ru>
2016-10-30 17:21:39 +0300

    make example more complex

--- src/app.js
+++ src/app.js
@@ -5,10 +5,18 @@ const animate = require("./console-animation");
 const populationToString = require("./population-to-string");
 const c = (x, y) => ({x, y});
 
+//@formatter:off
 let startPopulation = [
-            c(1, 0),
+               c(-13, 3),
+    c(-14, 4), c(-13, 4),c(-12, 4),
+
+    c(-17, 10), c(-16, 10),
+    c(-17, 11), c(-16, 11),
+
+             c(1, 0),
     c(0, 1),
     c(0, 2), c(1, 2), c(2, 2)];
+//@formatter:on
 
 let fieldDimension = {
     top: -1,
